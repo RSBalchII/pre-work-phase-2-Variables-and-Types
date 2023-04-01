66644fa204d02fec64047784d81754de44deb164
Robert Balch II <rbalchii@cnm.edu>
2023-03-31 19:35:13 -0600

    Create .gitignore
    Added file named .gitignore

---
+++ APIs-Object-and-Interfaces.js
@@ -0,0 +1,28 @@
+/*
+ * Objects, Interfaces, and API,s
+ */
+
+let dog = {
+    name: "steve",
+    color: "brown",
+    breed: "pug",
+    size: "small",
+    bark: function(typeOfBark){
+        console.log("Bark!")
+    },
+};
+
+/*
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+}
+
+x(y);
+console.log(y);
+*/
